790d117cf1fb2569f5e85d09ae8b12124ca40634
cauelima1 <cauezak@gmail.com>
2024-07-21 23:28:22 -0300

    desafios de ordenacao
    Signed-off-by: cauelima1 <cauezak@gmail.com>

---
+++ src/list/operecoesBasicas/ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,80 @@
+package list.operecoesBasicas.ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoNumeros  {
+    private List<Integer> numeros;
+
+
+
+    public OrdenacaoNumeros() {
+        this.numeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero){
+        numeros.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente(){
+            List<Integer> ordemCrescente = new ArrayList<>(numeros);
+            Collections.sort(ordemCrescente, new ComparatorCrescente());
+           return ordemCrescente;
+    }
+
+    public List<Integer> ordenarDescendente(){
+        List<Integer> ordemDecrescente = new ArrayList<>(numeros);
+        Collections.sort(ordemDecrescente, new ComparatorDecrescente());
+        return ordemDecrescente;
+    }
+
+//    public List<Integer> ordenarDescendente() {
+//        List<Integer> ordemDecrescente = new ArrayList<>(numeros);
+//        ordemDecrescente.sort(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer a, Integer b) {
+//                return b - a;
+//            }
+//        });
+//        return ordemDecrescente;
+//
+//    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumeros ordenacaoNumeros = new OrdenacaoNumeros();
+
+        ordenacaoNumeros.adicionarNumero(1);
+        ordenacaoNumeros.adicionarNumero(2);
+        ordenacaoNumeros.adicionarNumero(4);
+        ordenacaoNumeros.adicionarNumero(7);
+        ordenacaoNumeros.adicionarNumero(9);
+        ordenacaoNumeros.adicionarNumero(1);
+        ordenacaoNumeros.adicionarNumero(2);
+        ordenacaoNumeros.adicionarNumero(4);
+        ordenacaoNumeros.adicionarNumero(7);
+        ordenacaoNumeros.adicionarNumero(9);
+
+        System.out.println(ordenacaoNumeros.numeros);
+        System.out.println("Numeros em ordem crescente? " + ordenacaoNumeros.ordenarAscendente());
+        System.out.println("Numeros em ordem decrescente? " + ordenacaoNumeros.ordenarDescendente());
+
+    }
+}
+
+class ComparatorCrescente implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer a, Integer b) {
+        return a - b;
+    }
+}
+
+class ComparatorDecrescente implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer a, Integer b) {
+        return b - a;
+    }
+}
--- src/list/operecoesBasicas/ordenacao/Pessoa.java
+++ src/list/operecoesBasicas/ordenacao/Pessoa.java
@@ -1,9 +1,15 @@
 package list.operecoesBasicas.ordenacao;
 
-public class Pessoa {
+import java.util.Comparator;
+
+public class Pessoa implements Comparable<Pessoa> {
     private String nome;
     private int idade;
     private double altura;
+
+    public int compareTo(Pessoa p) {
+        return Integer.compare(idade, p.getIdade());
+    }
 
     public Pessoa(String nome, int idade, double altura) {
         this.nome = nome;
@@ -30,5 +36,14 @@ public class Pessoa {
                 ", idade=" + idade +
                 ", altura=" + altura +
                 '}';
+    }
+
+}
+
+class ComparatorPorAltura implements Comparator<Pessoa> {
+
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+        return Double.compare(p1.getAltura(), p2.getAltura());
     }
 }
