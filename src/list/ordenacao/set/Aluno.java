69e6f4076226940ba451f5f2da758d707f4b7534
cauelima1 <cauezak@gmail.com>
2024-07-23 01:36:08 -0300

    concluindo Collection com Set
    Signed-off-by: cauelima1 <cauezak@gmail.com>

---
+++ src/list/ordenacao/set/Aluno.java
@@ -0,0 +1,78 @@
+package list.ordenacao.set;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Aluno implements Comparable <Aluno> {
+    private String nome;
+    private long matricula;
+    private double nota;
+
+    public Aluno(String nome, long matricula, double nota) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    @Override
+    public int compareTo(Aluno aluno) {
+        return nome.compareTo(aluno.getNome());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return  "nome='" + nome + '\'' +
+                ", matricula=" + matricula +
+                ", nota=" + nota +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return getMatricula() == aluno.getMatricula();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getMatricula());
+    }
+
+
+
+}
+
+class ComparatorNota implements Comparator<Aluno> {
+
+
+    @Override
+    public int compare(Aluno a1, Aluno a2) {
+        return Double.compare(a1.getNota(), a2.getNota());
+    }
+}
