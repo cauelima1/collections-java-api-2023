58b0fd37debdf602a9fb279f61d667b00447ca65
cauelima1 <cauezak@gmail.com>
2024-07-25 17:37:13 -0300

    fim da aula de collections e generics
    Signed-off-by: cauelima1 <cauezak@gmail.com>

---
+++ src/Teste.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Teste {
+    static Set<String> coresDaBandeiraDoBrasil = new TreeSet<>(Arrays.asList("verde", "amarelo", "azul", "branco"));
+
+    public static void main(String[] args) {
+        System.out.println(coresDaBandeiraDoBrasil);
+    }
+
+
+}
